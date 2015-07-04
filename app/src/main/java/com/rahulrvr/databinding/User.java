47fc3cb0ef199bd6fccb361c88cffb40e776093f
Rahul RV <rahul.rv87@gmail.com>
2015-07-03 20:48:22 -0400

    Initial commit

---
+++ app/src/main/java/com/rahulrvr/databinding/User.java
@@ -0,0 +1,16 @@
+package com.rahulrvr.databinding;
+
+/**
+ *
+ *
+ */
+public class User {
+
+    public String name;
+    public String email;
+
+    public User(String name,String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
